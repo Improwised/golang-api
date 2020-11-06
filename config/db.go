11e52a221328bc47378f6d4eb753191de2a30321
aman00323 <48248836+aman00323@users.noreply.github.com>
2020-11-06 18:06:19 +0530

    Remove default

--- config/db.go
+++ config/db.go
@@ -2,13 +2,13 @@ package config
 
 // DBConfig type of db config object
 type DBConfig struct {
-	Host           string `envconfig:"DB_HOST" default:"localhost"`
+	Host           string `envconfig:"DB_HOST"`
 	Port           int    `envconfig:"DB_PORT"`
-	Username       string `envconfig:"DB_USERNAME" default:"root"`
-	Password       string `envconfig:"DB_PASSWORD" default:""`
+	Username       string `envconfig:"DB_USERNAME"`
+	Password       string `envconfig:"DB_PASSWORD"`
 	Db             string `envconfig:"DB_NAME"`
-	QueryString    string `envconfig:"DB_QUERYSTRING" default:"sslmode=disable"`
-	MigrationDir   string `required:"true" envconfig:"MIGRATION_DIR" default:"database/migrations"`
-	Dialect        string `required:"true" envconfig:"DB_DIALECT" default:"postgres"`
-	SQLiteFilePath string `envconfig:"SQLITE_FILE_PATH" default:"database/golang-api"`
+	QueryString    string `envconfig:"DB_QUERYSTRING"`
+	MigrationDir   string `required:"true" envconfig:"MIGRATION_DIR"`
+	Dialect        string `required:"true" envconfig:"DB_DIALECT"`
+	SQLiteFilePath string `envconfig:"SQLITE_FILEPATH"`
 }
