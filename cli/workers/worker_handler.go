e39978a2b35e6316119417a8edf5b595301f59f5
chintansakhiya <chintan.sakhiya@improwised.com>
2024-01-31 12:24:32 +0530

    fix: message payload set when job fail

--- cli/workers/worker_handler.go
+++ cli/workers/worker_handler.go
@@ -4,9 +4,15 @@ import (
 	"bytes"
 	"encoding/gob"
 	"encoding/json"
+	"strconv"
 
 	"github.com/ThreeDotsLabs/watermill/message"
 )
+
+type QConfig struct {
+	Topic      string
+	MaxRetries int
+}
 
 func init() {
 	for _, v := range RegisterWorkerStruct() {
@@ -28,7 +34,7 @@ type Handler interface {
 }
 
 // process all worker struct and call Handle function according to struct
-func Process(msg *message.Message) error {
+func (q QConfig) Process(msg *message.Message) error {
 	buf := bytes.NewBuffer(msg.Payload)
 	dec := gob.NewDecoder(buf)
 
@@ -37,14 +43,32 @@ func Process(msg *message.Message) error {
 	if err != nil {
 		return err
 	}
+
 	// Store the JSON payload in the msg so that it can be persisted in the database in case the job fails.
-	msg.Payload, err = json.Marshal(result)
-	if err != nil {
-		return err
+	CurrentRetryCount := GetSetRetryCount(msg)
+	if CurrentRetryCount == q.MaxRetries {
+		msg.Payload, err = json.Marshal(result)
+		if err != nil {
+			return err
+		}
 	}
+
 	if err := result.Handle(); err != nil {
 		return err
 	}
 	msg.Ack()
 	return nil
+}
+
+// set/get retry count from message metadata
+func GetSetRetryCount(msg *message.Message) int {
+	count := msg.Metadata.Get("max_retry_count")
+	if count == "" {
+		msg.Metadata.Set("max_retry_count", "0")
+		count = "0"
+		return 0
+	}
+	countInt, _ := strconv.Atoi(count)
+	msg.Metadata.Set("max_retry_count", strconv.Itoa(countInt+1))
+	return countInt + 1
 }
